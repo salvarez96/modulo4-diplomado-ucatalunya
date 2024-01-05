afb2095d0f57c4a7605d4c61664b6082c16a6acc
salvarez96 <santiagoalvarez9617@gmail.com>
2024-01-04 19:27:10 -0500

    Implement verify middleware to protect user endpoints
    Fix middleware functionality

--- src/middleware/verify.ts
+++ src/middleware/verify.ts
@@ -1,19 +1,18 @@
 import jwt from 'jsonwebtoken'
 import { SECRET_KEY } from '../config'
 import { NextFunction } from 'express'
+import { Request, Response } from 'express'
 
-export function verify(req: any, res: Response, next: NextFunction) {
+export function verify(req: Request, res: Response, next: NextFunction) {
   const token: any = req.headers.token
+  res.setHeader('Content-Type', 'application/json')
 
-  if(token) {
-    jwt.verify(token, SECRET_KEY, (err: any, decoded: any) => {
-      if (err) {
-        return res.status(500).json({message: 'Invalid token', error: err})
-      }
-      req.decoded = decoded
-      next()
-    })
-  }
+  if (!token) 
+    return res.status(401).json({message: 'Token mandatory'})
 
-  return res.status(403).json({message: 'You shall not pass >:v'})
+  jwt.verify(token, SECRET_KEY, (err: any, decoded: any) => {
+    if (err)
+      return res.status(401).json({message: 'Invalid token', error: err})
+    return next()
+  })
 }
--- src/routes/usuarios/usuarios.routes.ts
+++ src/routes/usuarios/usuarios.routes.ts
@@ -1,14 +1,14 @@
 import express from "express";
 import * as userController from "../../controllers/usuarios.controller";
+import { verify } from "../../middleware/verify";
 
 const usuariosRoutes = express.Router()
 
-usuariosRoutes.get('/', [userController.getUsuarios])
-usuariosRoutes.get('/:id', [userController.getUsuariosById])
-usuariosRoutes.post('/', [userController.createUsuarios])
-usuariosRoutes.post('/login', [userController.loginUsuario])
-usuariosRoutes.put('/:id', [userController.updateUsuarios])
-usuariosRoutes.delete('/:id', [userController.deleteUsuarios])
+usuariosRoutes.get('/', [verify, userController.getUsuarios])
+usuariosRoutes.get('/:id', [verify, userController.getUsuariosById])
+usuariosRoutes.post('/', [verify, userController.createUsuarios])
+usuariosRoutes.post('/login', [verify, userController.loginUsuario])
+usuariosRoutes.put('/:id', [verify, userController.updateUsuarios])
+usuariosRoutes.delete('/:id', [verify, userController.deleteUsuarios])
 
 export { usuariosRoutes }
-
